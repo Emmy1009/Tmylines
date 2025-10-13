e8621aa944aed6584fb840653553d34703d41fe2
Emmy1009 <emmygoldbusiness@gmail.com>
2025-10-13 14:10:20 +0100

    Update Messages.jsx

--- frontend/src/pages/admin/Messages.jsx
+++ frontend/src/pages/admin/Messages.jsx
@@ -31,7 +31,7 @@ export default function Messages() {
     if (!confirmDelete) return;
 
     try {
-      await axios.delete(`http://localhost:5000/api/messages/${id}`, {
+      await axios.delete(`https://tmylines-queh.onrender.com/api/messages/${id}`, {
         withCredentials: true,
       });
       toast.success("Message deleted successfully");
