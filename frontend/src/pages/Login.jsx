e09f8837691e904472ea9b5ad3613164b4dcaace
Emmy1009 <emmygoldbusiness@gmail.com>
2025-10-13 10:44:43 +0100

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://tmylines-queh.onrender.com/api/auth/login",
         { email, password },
         { withCredentials: true }
       );
